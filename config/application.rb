43f260d4512be0fbdb5233b9f0f975afccdf71ac
Casey Jenks <caseyjenks@gmail.com>
2014-09-26 14:30:46 -0400

    config stuff for staging

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Subscriply
   class Application < Rails::Application
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'controllers', '{**}')]
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
