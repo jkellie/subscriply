e75c9a772ea49cddbe353b82b97f623a02893188
Casey Jenks <caseyjenks@gmail.com>
2014-09-29 13:04:58 -0400

    avatar image fallback

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -14,7 +14,7 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
