b0d649f37df3222cc199db212d268c761370ccb0
Casey Jenks <caseyjenks@gmail.com>
2014-10-08 12:15:46 -0400

    filter passwords

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  :card_number,
+  :client_secret,
+  :password,
+  :password_confirmation,
+  :card_verification
+]
