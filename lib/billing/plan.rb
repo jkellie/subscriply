70f13695b54aa24e30f26aee033becc4da10a3ff
Casey Jenks <caseyjenks@gmail.com>
2014-10-14 20:03:37 -0400

    namespace billing module names

--- lib/billing/plan.rb
+++ lib/billing/plan.rb
@@ -1,31 +1,29 @@
-module Billing
-  
-  module Plan
-    def self.create(plan)
-      Billing.with_lock(plan.organization) do
-        Recurly::Plan.create(
-          plan_code: plan.permalink,
-          name: plan.name,
-          description: plan.description,
-          unit_amount_in_cents: plan.amount_in_cents,
-          plan_interval_length: plan.charge_every,
-          trial_interval_length: plan.free_trial_length,
-          trial_interval_unit: 'days'
-        )
-      end
-    end
+module Billing::Plan
 
-    def self.update(plan)
-      Billing.with_lock(plan.organization) do
-        _plan = Recurly::Plan.find(plan.permalink)
-        _plan.name = plan.name
-        _plan.description = plan.description
-        _plan.unit_amount_in_cents = plan.amount_in_cents
-        _plan.plan_interval_length = plan.charge_every
-        _plan.trial_interval_length = plan.free_trial_length
-        _plan.save
-      end
+  def self.create(plan)
+    Billing.with_lock(plan.organization) do
+      Recurly::Plan.create(
+        plan_code: plan.permalink,
+        name: plan.name,
+        description: plan.description,
+        unit_amount_in_cents: plan.amount_in_cents,
+        plan_interval_length: plan.charge_every,
+        trial_interval_length: plan.free_trial_length,
+        trial_interval_unit: 'days'
+      )
     end
   end
-  
+
+  def self.update(plan)
+    Billing.with_lock(plan.organization) do
+      _plan = Recurly::Plan.find(plan.permalink)
+      _plan.name = plan.name
+      _plan.description = plan.description
+      _plan.unit_amount_in_cents = plan.amount_in_cents
+      _plan.plan_interval_length = plan.charge_every
+      _plan.trial_interval_length = plan.free_trial_length
+      _plan.save
+    end
+  end
+
 end
--- lib/billing/subscription.rb
+++ lib/billing/subscription.rb
@@ -1,14 +1,12 @@
-module Billing
-  
-  module Subscription
-    def self.create(subscription, token)
-      Billing.with_lock(subscription.organization) do
-        Recurly::Subscription.create!(
-          plan_code: subscription.plan.permalink,
-          account: { account_code: subscription.user.uuid, billing_info: { token_id: token } }
-        )
-      end
+module Billing::Subscription
+
+  def self.create(subscription, token)
+    Billing.with_lock(subscription.organization) do
+      Recurly::Subscription.create!(
+        plan_code: subscription.plan.permalink,
+        account: { account_code: subscription.user.uuid, billing_info: { token_id: token } }
+      )
     end
   end
-
+  
 end
