88ca69382f74f9d7509d0c7ba910fdcc0684bc06
Casey Jenks <caseyjenks@gmail.com>
2014-10-16 12:17:36 -0400

    specs for billing subscriptions

--- lib/billing/subscription.rb
+++ lib/billing/subscription.rb
@@ -1,8 +1,12 @@
 module Billing::Subscription
 
+  def self.subscription_module
+    Recurly::Subscription
+  end
+
   def self.create(subscription, token)
     Billing.with_lock(subscription.organization) do
-      Recurly::Subscription.create!(
+      subscription_module.create!(
         plan_code: subscription.plan.permalink,
         account: { account_code: subscription.user.uuid, billing_info: { token_id: token } }
       )
---
+++ spec/lib/billing/subscription_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Billing::Subscription, '.create' do
+  let!(:subscription) { FactoryGirl.create(:subscription) }
+  let(:recurly_subscription) { double('Recurly::Subscription')}
+
+  before do
+    Billing::Subscription.stub(:subscription_module).and_return(recurly_subscription)
+    recurly_subscription.should_receive('create!').with(
+      {
+        plan_code: subscription.plan.permalink,
+        account: { account_code: subscription.user.uuid, billing_info: { token_id: '12345' } }  
+      }
+    )
+  end
+
+  subject do
+    Billing::Subscription.create(subscription, '12345')
+  end
+
+  it "calls recurly to create the subscription" do
+    subject
+  end
+end
