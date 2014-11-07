cd019236924530ebc408be16138abbdcd09373b8
Casey Jenks <caseyjenks@gmail.com>
2014-11-07 13:37:30 -0500

    dupe validation on subscription total records

--- app/models/subscription_total_record.rb
+++ app/models/subscription_total_record.rb
@@ -3,4 +3,6 @@ class SubscriptionTotalRecord < ActiveRecord::Base
   belongs_to :plan
 
   scope :between, ->(start_date, end_date) { where(created_at: start_date...end_date)}
+
+  validates :plan_id, uniqueness: {scope: [:organization_id, :created_at]}
 end
