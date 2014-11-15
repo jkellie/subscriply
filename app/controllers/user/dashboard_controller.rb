c1611bf82f721c85e02f07fabbefcd30d38b0827
Casey Jenks <caseyjenks@gmail.com>
2014-11-15 16:05:06 -0500

    require user for user dashboard

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class User::DashboardController < User::BaseController
+  before_action :authenticate_user!
+
   def show
     @dashboard_presenter = User::DashboardPresenter.new(user: current_user)
   end
