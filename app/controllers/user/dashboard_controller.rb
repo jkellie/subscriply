08128b77e51f0259fda6b2cdb5afcbd6279924e8
Casey Jenks <caseyjenks@gmail.com>
2014-11-17 11:32:02 -0500

    add back in splash page

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class User::DashboardController < User::BaseController
   before_action :authenticate_user!
+  layout 'splash'
 
   def show
     @dashboard_presenter = User::DashboardPresenter.new(user: current_user)
