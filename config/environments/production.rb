90a815bd91ed1b2efd86b226e8f2afc80df82f2d
Christopher Green <chris@bluesouth.org>
2015-02-25 17:33:01 -0500

    Setting default url for paths in emails in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'subscriply.com' }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
