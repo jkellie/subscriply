7cba2f069c829637f8e9bc05ab02948eea7d08b5
Christopher Green <chris@bluesouth.org>
2015-07-15 04:56:14 -0400

    Turning of splash.

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class User::DashboardController < User::BaseController
-  # before_action :authenticate_user!
-  layout 'splash'
+  before_action :authenticate_user!
+  # layout 'splash'
 
   def show
-    render 'splash'
-    #@dashboard_presenter = User::DashboardPresenter.new(user: current_user)
+    # render 'splash'
+    @dashboard_presenter = User::DashboardPresenter.new(user: current_user)
   end
 end
